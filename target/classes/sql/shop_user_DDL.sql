51fc9339d7e37549280522ac05c1c48e45b66128
OnionchipBox <10823syn@naver.com>
2023-09-05 16:15:11 +0900

    매퍼 수정

--- target/classes/sql/shop_user_DDL.sql
+++ target/classes/sql/shop_user_DDL.sql
@@ -6,16 +6,16 @@ DROP TABLE shop_user;
 create table shop_user
 (
   ID        VARCHAR2(50)          PRIMARY KEY,
-  PW        VARCHAR2(100)        ,
+  PASSWORD        VARCHAR2(100)        ,
   NAME      VARCHAR2(30)                  ,
   GENDER      VARCHAR2(10)                  ,
   PHONE     VARCHAR2(20)                ,
   EMAIL     VARCHAR2(30)                 ,
   NICKNAME VARCHAR2(30)                ,
-  ADDRESS   VARCHAR2(20)                  ,
-  BIRTHDAY  DATE                  ,
-  JOINDATE  VARCHAR2(20)                  ,
-  UPDATEDATE VARCHAR2(20)
+  ADDRESS   VARCHAR2(50)                  ,
+  BIRTHDAY  VARCHAR2(30)                 ,
+  JOINDATE  DATE                  ,
+  UPDATEDATE DATE
 );
 
 -------------------------------------------------------------------------------- 
